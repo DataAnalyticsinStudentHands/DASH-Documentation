d055e33d0ddb0ef0b5e8a1630cb3ba831d03e956
Binh Tran <o.otuto@gmail.com>
2015-09-09 11:27:30 -0500

    Revert "Merge thinimaging scripts"
    This reverts commit 0121291033a2b3eef801245cc70875253d92cdff.

--- Honors IT/scripts/disable_screen_lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-osascript="/usr/bin/osascript"
-
-
-osascript -e 'tell application "System Events" to set require password to wake of security preferences to false'
-#Doesn't work
-#defaults write com.apple.screensaver askForPassword -int 0
--- Honors IT/scripts/reset_keychains.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/usr/bin/find /Users -name Keychains -maxdepth 3 -mindepth 1 -type d -print -exec rm -rfv {} \;
-reboot
