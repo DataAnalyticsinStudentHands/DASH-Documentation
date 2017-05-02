54e3abc7525d967055b6a4a61a46aa49dc22c697
Moorman, Nicholas V <nvmoorman@uh.edu>
2017-05-02 17:02:09 -0500

    For the lab printers

---
+++ Honors IT/scripts/set_default_printer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+defaults write org.cups.PrintingPrefs UseLastPrinter -bool False
+#this needs to be false because it takes precedence over defaults
+
+if [[ $(hostname) == *"bA"* || $(hostname) == *"bB"* ||
+      $(hostname) == *"bC"* || $(hostname) == *"bD"* ||
+      $(hostname) == *"bE"* || $(hostname) == *"bF"* ||
+      $(hostname) == *"bG"* || $(hostname) == *"bH"* ]]
+then
+  lpoptions -d mcx_0
+else
+  lpoptions -d mcx_1
+fi
