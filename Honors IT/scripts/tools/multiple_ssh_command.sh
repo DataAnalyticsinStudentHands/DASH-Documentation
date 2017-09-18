debf145247a83e80783151e94b8223b557653427
Moorman, Nicholas V <nvmoorman@uh.edu>
2017-09-18 17:27:07 -0500

    no message

--- Honors IT/scripts/tools/multiple_ssh_command.sh
+++ Honors IT/scripts/tools/multiple_ssh_command.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#replace the list of random IPs with the desired list
+#replace the list of lab computer IPs with the desired list
 #replace "some command; another command" the desired command(s), separated by semicolon
 #note that this script is not perfect and is best for simple commands. It may exit the ssh before a more involved process finishes
 
 USERNAME=hcadmin
-HOSTS="172.27.219.113 172.27.56.238 172.27.219.191 172.27.56.188 172.27.56.176 172.27.56.62 172.27.56.60"
+HOSTS="172.27.56.195 172.27.56.167 172.27.56.225 172.27.56.222 172.27.56.224 172.27.56.240 172.27.56.153 172.27.56.218 172.27.56.203 172.27.56.237 172.27.56.151 172.27.56.223 172.27.56.158 172.27.56.157 172.27.56.248 172.27.56.234"
 SCRIPT="some command; sudo another command"
 for HOSTNAME in ${HOSTS} ; do
     ssh -t -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
